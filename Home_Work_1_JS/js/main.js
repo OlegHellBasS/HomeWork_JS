d9b3b5e18d49c7c68995189432840027c13d9b3a
OlegHellBasS <oleghellbass1@gmail.com>
2022-11-01 19:52:34 +0200

    Initial commit

---
+++ Home_Work_1_JS/js/main.js
@@ -0,0 +1,36 @@
+let helo = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let num_1 = 1;
+let num_10 = 10;
+let num__999 = -999;
+let num_123 = 123;
+let num_3_14 = 3.14;
+let num_2_7 = 2.7;
+let num_16 = 16;
+let boolyan_true = true;
+let boolyan_false = false;
+console.log(helo, owu, com, ua, num_1, num_10, num__999, num_123, num_3_14, num_2_7, num_16, boolyan_true, boolyan_false);
+
+let firstName = 'Oleg'
+let middleName = 'Mykhailovych'
+let lastName = 'Podusovsky'
+let person = `${firstName} ${middleName} ${lastName}`
+// let person = firstName + ' ' + middleName + ' ' + lastName
+console.log(person)
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a, typeof b, typeof c)
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+//
+let name= prompt('What is your name?','');
+let midName= prompt('middle Name?','');
+let n= Number(prompt('\n'+'How old are you?:',''));
+alert('Hello '+name+' !');
+console.log(name,midName,n)
+
