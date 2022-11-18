67eb8e37359945770370f3c1189c7c252d696bc0
OlegHellBasS <oleghellbass1@gmail.com>
2022-11-18 04:48:43 +0200

    JS Homework 5 4

--- Home_Work_5_JS/js/functions.js
+++ Home_Work_5_JS/js/functions.js
@@ -19,12 +19,27 @@ console.log(cilinder(6, 8))
 
 //--------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка приймає масив та виводить кожен його елемент
-// function f_arr_1 (arr){
-//     for (const arrElement of arr) {
-//         console.log(arrElement)
-//     }
-// }
-// f_arr_1([1,2,3,4,5,6])
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+function f_arr_1 (arr){
+    for (const arrElement of arr) {
+        console.log(arrElement)
+    }
+}
+f_arr_1(users)
 
 //--------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
