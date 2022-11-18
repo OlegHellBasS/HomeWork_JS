a1941586cdb7708fd5f0fee97200fea08841c200
OlegHellBasS <oleghellbass1@gmail.com>
2022-11-18 08:14:33 +0200

    JS Homework 5 8

--- Home_Work_5_JS/js/functions.js
+++ Home_Work_5_JS/js/functions.js
@@ -1,13 +1,19 @@
 // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
 
-function rectangle(a, b) {return a * 2 + b * 2;}
+function rectangle(a, b) {
+    return a * 2 + b * 2;
+}
+
 let a = rectangle(10, 20)
 console.log(a)
 
 //--------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
 
-function radius(r) {return Math.PI * Math.pow(r, 2);}
+function radius(r) {
+    return Math.PI * Math.pow(r, 2);
+}
+
 let b = radius(10)
 console.log(b)
 
@@ -21,44 +27,47 @@ console.log(cilinder(6, 8))
 // - створити функцію яка приймає масив та виводить кожен його елемент
 
 let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
+    {id: 1, name: 'vasya', age: 31, status: false},
+    {id: 2, name: 'petya', age: 30, status: true},
+    {id: 3, name: 'kolya', age: 29, status: true},
+    {id: 4, name: 'olya', age: 28, status: false},
+    {id: 5, name: 'max', age: 30, status: true},
+    {id: 6, name: 'anya', age: 31, status: false},
+    {id: 7, name: 'oleg', age: 28, status: false},
+    {id: 8, name: 'andrey', age: 29, status: true},
+    {id: 9, name: 'masha', age: 30, status: true},
+    {id: 10, name: 'olya', age: 31, status: false},
+    {id: 11, name: 'max', age: 31, status: true}
 ];
 
-function f_arr_1 (arr){
+function f_arr_1(arr) {
     for (const arrElement of arr) {
         console.log(arrElement)
     }
 }
+
 f_arr_1(users)
 
 //--------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
 
-function paragraf (city){
+function paragraf(city) {
     document.write(`<p>${city}</p>`)
 }
+
 paragraf('Львів')
 
 //--------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
 
-function ul_li_3 (name){
+function ul_li_3(name) {
     document.write(`<ul>`)
     document.write(`<li>${name}</li>
                     <li>${name}</li>
                     <li>${name}</li>`)
     document.write(`</ul>`);
 }
+
 ul_li_3('Oleg')
 
 //--------------------------------------------------------------------------------------------------------------------
@@ -72,12 +81,53 @@ function ul_li_c(text, num) {
         document.write(`</ul>`);
     }
 }
-ul_li_c('City Lviv',5)
+
+ul_li_c('City Lviv', 5)
 
 //--------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 
+let arr_num_str = [true, 'stop', 6, 'play', false];
+
+function arr_ul_li_item(arr) {
+    document.write(`<ul>`)
+    for (const arrElement of arr) {
+        document.write(`<li>${arrElement}</li>`)
+    }
+    document.write(`</ul>`)
+}
+
+arr_ul_li_item(arr_num_str)
+
 //--------------------------------------------------------------------------------------------------------------------
-// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ.
+// Для кожного об'єкту окремий блок.
+
+function arr_id_name_age(arr) {
+    for (const arrElement of arr) {
+        document.write(`<div>${arrElement.id} ${arrElement.name} ${arrElement.age}</div>`)
+    }
+}
+
+arr_id_name_age(users)
+//--------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка повертає найменьше число з масиву
+
+let arr_2 = [21, 17, 13, 6, 22, 31, 45, 66, 100, 18];
+
+function arr_min_num(arr) {
+    let min_num = arr[0];
+    for (const arrNum of arr) {
+        if (min_num > arrNum) {
+            min_num = arrNum
+        }
+    }return min_num
+
+}
+let x = arr_min_num(arr_2);
+console.log(x)
+
+//--------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+
+//--------------------------------------------------------------------------------------------------------------------
