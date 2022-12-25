70f3052c6e0682a885f338cd62436d97c589e91e
OlegHellBasS <oleghellbass1@gmail.com>
2022-12-25 06:45:35 +0200

    JS Homework 8 5

--- Home_Work_8_JS/js/main.js
+++ Home_Work_8_JS/js/main.js
@@ -6,7 +6,7 @@ function User(id, name, surname, email, phone) {
     this.email = email;
     this.phone = phone;
 }
-
+//-------------------------------------------------------------------------------------------------------------//
 // створити пустий масив, наповнити його 10 об'єктами new User(....)
 let users = [
     new User(1, 'Margot', 'Robbie', 'margotr@gmail.com', 6139866),
@@ -21,11 +21,13 @@ let users = [
     new User(10, 'Rebecca', 'Hansen', 'rebeccahansen@gmail.com', 463456465),
 ];
 console.log(users)
+//-------------------------------------------------------------------------------------------------------------//
 // - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
 console.log(users.filter(value => value.id % 2 === 0));
+//-------------------------------------------------------------------------------------------------------------//
 // - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
 console.log(users.sort((a, b) => b.id - a.id));
-
+//-------------------------------------------------------------------------------------------------------------//
 // - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
 class Client {
     constructor(id, name, surname, email, phone, order) {
@@ -37,6 +39,7 @@ class Client {
         this.order = order
     }
 }
+//-------------------------------------------------------------------------------------------------------------//
 // створити пустий масив, наповнити його 10 об'єктами Client
 clients = [
     new Client(1,'Margot', 'Robbie', 'margotr@gmail.com', 6139866,['Film','Television','Music videos']),
@@ -51,8 +54,10 @@ clients = [
     new Client(10,'Rebecca', 'Hansen', 'rebeccahansen@gmail.com', 463456465,['Television'])
 ];
 console.log(clients)
+//-------------------------------------------------------------------------------------------------------------//
 // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 console.log(clients.sort((a, b) => a.order.length - b.order.length));
+//-------------------------------------------------------------------------------------------------------------//
 // - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску,
 // максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
 // -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
@@ -68,7 +73,7 @@ function Car (model,producer,year,max_speed,engine_capacity){
     this.engine_capacity = engine_capacity
 
     this.drive = function () {
-        console.log(`їдемо зі швидкістю ${max_speed} на годину`)
+        console.log(`їдемо зі швидкістю ${this.max_speed} на годину`)
     }
 
     this.info = function () {
@@ -93,7 +98,8 @@ car.drive()
 car.info()
 car.increaseMaxSpeed(140)
 car.changeYear(1965)
-car.addDriver('Spidi')
+car.addDriver({name:'spidi',age:33})
+//-------------------------------------------------------------------------------------------------------------//
 // - (Те саме, тільки через клас)
 // Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
 // -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
@@ -101,10 +107,70 @@ car.addDriver('Spidi')
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
-//
-//
-//
+class Car_2 {
+    constructor(model, producer, year, max_speed, engine_capacity) {
+        this.model = model;
+        this.producer = producer;
+        this.year = year;
+        this.max_speed = max_speed;
+        this.engine_capacity = engine_capacity;
+    }
+    drive = function () {
+        console.log(`їдемо зі швидкістю ${this.max_speed} на годину`)
+    }
+
+    info = function () {
+        console.log(this.model,this.producer,this.year,this.max_speed,this.engine_capacity)
+    }
+
+    increaseMaxSpeed = function (newSpeed) {
+        this.max_speed += newSpeed
+    }
+
+    changeYear = function (newValue) {
+        this.year = newValue
+    }
+
+    addDriver = function (driver) {
+        this.driver = driver
+    }
+}
+let car_p = new Car_2 ('Porsche 356','Porsche GmbH',1948,140,'1.1 L B4, 40 hp (DIN)');
+car_p.drive()
+car_p.info()
+car_p.increaseMaxSpeed(140)
+car_p.changeYear(1965)
+car_p.addDriver({name:'spidi',age:33})
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
+class Cinderella {
+    constructor(name, age, foot_size) {
+        this.name = name;
+        this.age = age;
+        this.foot_size = foot_size;
+    }
+}
+const cinderellas = [
+    new Cinderella('Oksana',33,36),
+    new Cinderella('Olena',33,37),
+    new Cinderella('Natalia',33,38),
+    new Cinderella('Olya',35,35),
+    new Cinderella('Vira',36,40),
+    new Cinderella('Diana',26,37),
+    new Cinderella('Yulia',30,36),
+    new Cinderella('Katya',30,33),
+    new Cinderella('Vika',35,32),
+    new Cinderella('Mariana',30,39),
+    new Cinderella('Ira',33,37)
+]
+//-------------------------------------------------------------------------------------------------------------//
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
+class Prince {
+constructor(name, age, the_shoe_he_found) {
+        this.name = name;
+        this.age = age;
+        this.the_shoe_he_found = the_shoe_he_found;
+    }
+}
+new Prince ('Vasia',33,32)
 //     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
 //     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
